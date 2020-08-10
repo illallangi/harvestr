47543d7d9dd04efe8fa913eddacf88812d6cd3b1
Andrew Cole <andrew.cole@illallangi.com>
2020-08-10 14:26:47 +1000

    Added creation of recycle and target dirs

--- harvestr/__main__.py
+++ harvestr/__main__.py
@@ -4,12 +4,14 @@ import click
 from .harvestr import Harvestr
 
 @click.command(context_settings={"auto_envvar_prefix": "HARVESTR"})
-@click.option('--target',  '-t', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
-@click.option('--recycle', '-r', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False))
+@click.option('--target',  '-t', type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
+@click.option('--recycle', '-r', type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False))
 @click.option('--source',  '-s', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=False, readable=True, resolve_path=True, allow_dash=False), required=True, multiple=True)
 @click.option('--dry-run', '-d', is_flag=True)
 @click.option('--verbose', '-v', is_flag=True)
 def main(target, dry_run, verbose, recycle=None, source=None):
+  os.makedirs(target, exist_ok=True)
+  os.makedirs(recycle, exist_ok=True)
   h = Harvestr(target, recycle, source, dry_run = dry_run, verbose = verbose)
   h.main()
   schedule.every(1).minute.do(h.main)
