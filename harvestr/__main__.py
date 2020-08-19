12e6933f97f0aad0390c2ad93db3e78d2a4fd883
Andrew Cole <andrew.cole@illallangi.com>
2020-08-19 12:24:11 +1000

    Specified imports from click

--- harvestr/__main__.py
+++ harvestr/__main__.py
@@ -2,27 +2,27 @@ from os import makedirs
 from sys import stderr
 from time import sleep
 
-import click
+from click import Choice as CHOICE, INT, Path as PATH, command, option
 
 from loguru import logger
 
 from .harvestr import Harvestr
 
 
-@click.command(context_settings={"auto_envvar_prefix": "HARVESTR"})
-@click.option('--target', '-t', type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
-@click.option('--recycle', '-r', type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
-@click.option('--source', '-s', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=False, readable=True, resolve_path=True, allow_dash=False), required=True, multiple=True)
-@click.option('--dry-run', '-d', is_flag=True)
-@click.option('--log-level',
-              type=click.Choice(['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'SUCCESS', 'TRACE'],
-                                case_sensitive=False),
-              envvar='HARVESTR_LOG_LEVEL',
-              default='DEBUG')
-@click.option('--sleep-time',
-              type=click.INT,
-              envvar='HARVESTR_SLEEP_TIME',
-              default=5)
+@command(context_settings={"auto_envvar_prefix": "HARVESTR"})
+@option('--target', '-t', type=PATH(exists=False, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
+@option('--recycle', '-r', type=PATH(exists=False, file_okay=False, dir_okay=True, writable=True, readable=True, resolve_path=True, allow_dash=False), required=True)
+@option('--source', '-s', type=PATH(exists=True, file_okay=False, dir_okay=True, writable=False, readable=True, resolve_path=True, allow_dash=False), required=True, multiple=True)
+@option('--dry-run', '-d', is_flag=True)
+@option('--log-level',
+        type=CHOICE(['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'SUCCESS', 'TRACE'],
+                    case_sensitive=False),
+        envvar='HARVESTR_LOG_LEVEL',
+        default='DEBUG')
+@option('--sleep-time',
+        type=INT,
+        envvar='HARVESTR_SLEEP_TIME',
+        default=5)
 def main(target, dry_run, recycle, source, sleep_time, log_level):
     logger.remove()
     logger.add(stderr, level=log_level)
